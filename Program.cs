7c436aeb072eff92086eff044b33915e6e420601
Max-Gabriel-Susman <gabe.susman@gmail.com>
2025-04-27 03:38:38 -0600

    modified the service to only use swagger in it's dev environment

--- Program.cs
+++ Program.cs
@@ -11,7 +11,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.DocumentPath = "/openapi/v1.josn";
+    });
+
 }
 
 app.UseHttpsRedirection();
